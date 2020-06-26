be29dd22c35bfd96aa0206183a276f0e19c392e9
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-06-26 14:10:16 -0700

    styled page one of RulesView

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import styles from '../stylesheets/NavBar.module.css'
 
-// const navBarStyle = {
-//   height: "50px",
-//   width: "320px",
-//   backgroundColor: "#84C0F7",
-//   margin: "0px auto"
-// }
-
 function NavBar() {
   return(
     <div className={styles.navBar}>
--- src/components/RulesView.js
+++ src/components/RulesView.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
 import styles from '../stylesheets/RulesView.module.css'
 
-const rulesStyles = {
-  height: "518px",
-  width: "320px",
-  backgroundColor: "#D8D8D8",
-  margin: "0px auto"
-}
-
-const h1Style = {
-  margin: "0"
-}
 
 export default class RulesView extends Component {
   
@@ -23,9 +13,19 @@ export default class RulesView extends Component {
       return (
         <div className={styles.rulesDiv}>
           <h1 className={styles.h1}>Rules</h1>
-          {this.state.page === 1? <p>rules 1</p> : <p>rules 2</p>}
+          {this.state.page === 1? <p className={styles.p}>{page1}</p> : <p>page 2</p>}
+          <div className={styles.buttonsFooter}>
+            <input type="button" className={styles.lastButton} value="<"></input>
+            <input type="button" className={styles.nextButton} value=">"></input>
+            <br></br>
+            <input type="button" className={styles.homeButton} value="Home"></input>
+          </div>
         </div>
       );
     }
   }
 }
+
+const page1 = "Fishbowl is a 3-round parlour-style game. 4 or more \    players split into two teams. Teams take turns with a performer from \ their team trying to get their teammates to guess what is written on \ player-submitted cards randomly selected from a collective \
+'fishbowl.' While the cards are reused, the same each round differs \
+in performance rules. The team with the most points wins."
