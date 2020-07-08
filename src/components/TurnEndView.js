0f8c5bf543533993bf1705d6c94e2e255f328c70
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-07-08 09:29:41 -0700

    added styling to TurnEndView

--- src/components/TurnEndView.js
+++ src/components/TurnEndView.js
@@ -8,10 +8,11 @@ class TurnEndView extends Component {
   render(){
     return(
       <>
+        <h1 className={styles.timesUp}>Time's Up!</h1>
         <h3 className={styles.h3}>Hayao got <u>4</u> points</h3>
         <h2 className={styles.h2}>Score:</h2>
         <h3 className={styles.teamAName}>Team A: The Sharks</h3>
-        <h3 className={styles.teamBName}>Team A: The Jets</h3>
+        <h3 className={styles.teamBName}>Team B: The Jets</h3>
         <h1 className={styles.teamAScore}>12</h1>
         <h1 className={styles.teamBScore}>6</h1>
         <h3 className={styles.cardsLeft}>Cards Left in Fishbowl:</h3>
